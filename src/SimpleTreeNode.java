964e1d6a7d8df0ed486b2785329f5664814734a5
D0M1N1KUS <dominikus-noah@live.de>
2019-10-18 19:26:02 +0200

    Initial commit

---
+++ src/SimpleTreeNode.java
@@ -0,0 +1,14 @@
+package src;
+//Define tree element with no children
+
+public class SimpleTreeNode extends TreeNode  {
+
+  public SimpleTreeNode(String label) {
+    super(label);
+  }
+
+  public void inFixPrint()  {
+
+  }
+  
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,13 @@
+package src;
+//abstract superclass of tree element types
+
+public abstract class TreeNode  {
+
+  protected TreeNode(String label)  {
+    this.label = label;
+  }
+
+  public abstract void inFixPrint();
+
+  protected String label;
+}
